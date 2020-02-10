2c20baeff96b38a32c6ae4bd253fa4e355bbe187
Florian Adonis <tanohzana@gmail.com>
2020-02-10 16:22:20 +0100

    test URL

--- src/assets/sms-templates.js
+++ src/assets/sms-templates.js
@@ -1,7 +1,7 @@
 const createMessage = (templateId, user) => {
   if (templateId === 'paid-basket') {
     return `
-      Bonjour,\n\nVotre proche ${user.firstname} ${user.lastname} vous a fait parvenir un panier via MYFA.\n\nMYFA est la nouvelle solution permettant à vos proches de vous faire plaisir.\n\nNotre équipe vous contactera pour régler les modalités de livraison.\n\nAu plaisir,\n\nL'équipe MYFA
+      Bonjour,\n\nVotre proche ${user.firstname} ${user.lastname} vous a fait parvenir un panier via MYFA.\n\nMYFA est la nouvelle solution permettant à vos proches de vous faire plaisir tout au long de l'année.\n\nNotre équipe vous contactera pour régler les modalités de livraison.\n\nAu plaisir,\n\nL'équipe MYFA
     `;
   }
 };
