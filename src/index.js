58c1e49197c50be42a32bc4472837ca1c69122a5
Florian Adonis <tanohzana@gmail.com>
2020-02-10 17:26:53 +0100

    test URL

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ var whitelist = [
   'https://5e41242ca1d1cc00099c18cb--compassionate-varahamihira-d667c0.netlify.com',
   'https://5e415e53dd93350008492aa6--compassionate-varahamihira-d667c0.netlify.com',
   'https://5e4172ef4c0d9800081b3c72--compassionate-varahamihira-d667c0.netlify.com',
+  'https://5e418384b32c4f0007e2bcf8--compassionate-varahamihira-d667c0.netlify.com',
 ]
 
 const corsOptions = {
