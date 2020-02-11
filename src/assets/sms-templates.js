7d23aeb7058670d926aa1c30bae31d11a59bb403
Florian Adonis <tanohzana@gmail.com>
2020-02-11 13:35:05 +0100

    edit confirmation SMS

--- src/assets/sms-templates.js
+++ src/assets/sms-templates.js
@@ -1,7 +1,7 @@
 const createMessage = (templateId, user) => {
   if (templateId === 'paid-basket') {
     return `
-      Bonjour,\n\nVotre proche ${user.firstname} ${user.lastname} vous a fait parvenir un panier via MYFA.\n\nMYFA est la nouvelle solution permettant à vos proches de vous faire plaisir tout au long de l'année.\n\nNotre équipe vous contactera pour régler les modalités de livraison.\n\nAu plaisir,\n\nL'équipe MYFA
+      Bonjour,\n\nVotre proche ${user.firstname} ${user.lastname} vous a fait parvenir un panier via MYFA.\n\nMYFA est la nouvelle solution permettant à vos proches de vous faire plaisir tout au long de l'année : la livraison se fera le mois prochain par Doris et Florian, co-fondateurs de MYFA.\n\nNous prendrons contact avec vous pour régler les modalités de livraison.\n\nAu plaisir,\n\nL'équipe MYFA
     `;
   }
 };
