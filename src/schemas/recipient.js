356a1325ea2e37b8a3834ba8c97ec606155ed02e
Florian Adonis <tanohzana@gmail.com>
2020-02-21 11:17:37 +0100

    add other relation into recipient

--- src/schemas/recipient.js
+++ src/schemas/recipient.js
@@ -4,6 +4,7 @@ const RecipientSchema = mongoose.Schema({
   firstname: String,
   lastname: String,
   relation: String,
+  otherRelation: String,
   address: String,
   email: String,
   country: String,
