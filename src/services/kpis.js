afa07a0a2d3e658527af052eb69effc49235b4ee
Florian Adonis <tanohzana@gmail.com>
2020-03-19 18:17:45 +0000

    fetch KPIs paid or delivered

--- src/services/kpis.js
+++ src/services/kpis.js
@@ -20,25 +20,25 @@ const fetchKPIs = async (req, res, next) => {
 
     const rules = [
       { section: 'general', collection: 'users', filter: {}, type: 'count', id: 'nb_users', label: 'utilisateurs', model: userModel },
-      { section: 'general', collection: 'baskets', filter: { status: 'paid', type: 'fruits' }, id: 'nb_paid_fruits_baskets', type: 'count', label: 'paniers fruits', model: basketModel },
-      { section: 'general', collection: 'baskets', filter: { status: 'paid', type: 'legumes' }, id: 'nb_paid_legumes_baskets', type: 'count', label: 'paniers legumes', model: basketModel },
-      { section: 'general', collection: 'baskets', filter: { status: 'paid', type: 'sauces' }, id: 'nb_paid_sauces_baskets', type: 'count', label: 'paniers sauces', model: basketModel },
-      { section: 'general', collection: 'baskets', filter: { status: 'paid', type: 'myfa' }, id: 'nb_paid_myfa_baskets', type: 'count', label: 'paniers myfa', model: basketModel },
+      { section: 'general', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'fruits' }, id: 'nb_paid_fruits_baskets', type: 'count', label: 'paniers fruits', model: basketModel },
+      { section: 'general', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'legumes' }, id: 'nb_paid_legumes_baskets', type: 'count', label: 'paniers legumes', model: basketModel },
+      { section: 'general', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'sauces' }, id: 'nb_paid_sauces_baskets', type: 'count', label: 'paniers sauces', model: basketModel },
+      { section: 'general', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'myfa' }, id: 'nb_paid_myfa_baskets', type: 'count', label: 'paniers myfa', model: basketModel },
 
-      { section: 'week', collection: 'baskets', filter: { status: 'paid', type: 'fruits', createdAt: { $gte: weekMonday, $lte: weekSunday } }, id: 'week_nb_paid_fruits_baskets', type: 'count', label: 'paniers fruits', model: basketModel },
-      { section: 'week', collection: 'baskets', filter: { status: 'paid', type: 'legumes', createdAt: { $gte: weekMonday, $lte: weekSunday } }, id: 'week_nb_paid_legumes_baskets', type: 'count', label: 'paniers legumes', model: basketModel },
-      { section: 'week', collection: 'baskets', filter: { status: 'paid', type: 'sauces', createdAt: { $gte: weekMonday, $lte: weekSunday } }, id: 'week_nb_paid_sauces_baskets', type: 'count', label: 'paniers sauces', model: basketModel },
-      { section: 'week', collection: 'baskets', filter: { status: 'paid', type: 'myfa', createdAt: { $gte: weekMonday, $lte: weekSunday } }, id: 'week_nb_paid_myfa_baskets', type: 'count', label: 'paniers myfa', model: basketModel },
+      { section: 'week', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'fruits', createdAt: { $gte: weekMonday, $lte: weekSunday } }, id: 'week_nb_paid_fruits_baskets', type: 'count', label: 'paniers fruits', model: basketModel },
+      { section: 'week', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'legumes', createdAt: { $gte: weekMonday, $lte: weekSunday } }, id: 'week_nb_paid_legumes_baskets', type: 'count', label: 'paniers legumes', model: basketModel },
+      { section: 'week', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'sauces', createdAt: { $gte: weekMonday, $lte: weekSunday } }, id: 'week_nb_paid_sauces_baskets', type: 'count', label: 'paniers sauces', model: basketModel },
+      { section: 'week', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'myfa', createdAt: { $gte: weekMonday, $lte: weekSunday } }, id: 'week_nb_paid_myfa_baskets', type: 'count', label: 'paniers myfa', model: basketModel },
 
-      { section: 'month', collection: 'baskets', filter: { status: 'paid', type: 'fruits', createdAt: { $gte: monthFirstDay, $lte: monthLastDay } }, id: 'month_nb_paid_fruits_baskets', type: 'count', label: 'paniers fruits', model: basketModel },
-      { section: 'month', collection: 'baskets', filter: { status: 'paid', type: 'legumes', createdAt: { $gte: monthFirstDay, $lte: monthLastDay } }, id: 'month_nb_paid_legumes_baskets', type: 'count', label: 'paniers legumes', model: basketModel },
-      { section: 'month', collection: 'baskets', filter: { status: 'paid', type: 'sauces', createdAt: { $gte: monthFirstDay, $lte: monthLastDay } }, id: 'month_nb_paid_sauces_baskets', type: 'count', label: 'paniers sauces', model: basketModel },
-      { section: 'month', collection: 'baskets', filter: { status: 'paid', type: 'myfa', createdAt: { $gte: monthFirstDay, $lte: monthLastDay } }, id: 'month_nb_paid_myfa_baskets', type: 'count', label: 'paniers myfa', model: basketModel },
+      { section: 'month', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'fruits', createdAt: { $gte: monthFirstDay, $lte: monthLastDay } }, id: 'month_nb_paid_fruits_baskets', type: 'count', label: 'paniers fruits', model: basketModel },
+      { section: 'month', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'legumes', createdAt: { $gte: monthFirstDay, $lte: monthLastDay } }, id: 'month_nb_paid_legumes_baskets', type: 'count', label: 'paniers legumes', model: basketModel },
+      { section: 'month', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'sauces', createdAt: { $gte: monthFirstDay, $lte: monthLastDay } }, id: 'month_nb_paid_sauces_baskets', type: 'count', label: 'paniers sauces', model: basketModel },
+      { section: 'month', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'myfa', createdAt: { $gte: monthFirstDay, $lte: monthLastDay } }, id: 'month_nb_paid_myfa_baskets', type: 'count', label: 'paniers myfa', model: basketModel },
 
-      { section: 'today', collection: 'baskets', filter: { status: 'paid', type: 'fruits', createdAt: { $gte: today, $lte: tomorrow } }, id: 'today_nb_paid_fruits_baskets', type: 'count', label: 'paniers fruits', model: basketModel },
-      { section: 'today', collection: 'baskets', filter: { status: 'paid', type: 'legumes', createdAt: { $gte: today, $lte: tomorrow } }, id: 'today_nb_paid_legumes_baskets', type: 'count', label: 'paniers legumes', model: basketModel },
-      { section: 'today', collection: 'baskets', filter: { status: 'paid', type: 'sauces', createdAt: { $gte: today, $lte: tomorrow } }, id: 'today_nb_paid_sauces_baskets', type: 'count', label: 'paniers sauces', model: basketModel },
-      { section: 'today', collection: 'baskets', filter: { status: 'paid', type: 'myfa', createdAt: { $gte: today, $lte: tomorrow } }, id: 'today_nb_paid_myfa_baskets', type: 'count', label: 'paniers myfa', model: basketModel },
+      { section: 'today', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'fruits', createdAt: { $gte: today, $lte: tomorrow } }, id: 'today_nb_paid_fruits_baskets', type: 'count', label: 'paniers fruits', model: basketModel },
+      { section: 'today', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'legumes', createdAt: { $gte: today, $lte: tomorrow } }, id: 'today_nb_paid_legumes_baskets', type: 'count', label: 'paniers legumes', model: basketModel },
+      { section: 'today', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'sauces', createdAt: { $gte: today, $lte: tomorrow } }, id: 'today_nb_paid_sauces_baskets', type: 'count', label: 'paniers sauces', model: basketModel },
+      { section: 'today', collection: 'baskets', filter: { $or: [{ status: 'paid' }, { status: 'delivered' }], type: 'myfa', createdAt: { $gte: today, $lte: tomorrow } }, id: 'today_nb_paid_myfa_baskets', type: 'count', label: 'paniers myfa', model: basketModel },
     ];
 
     rules.forEach(rule => {
