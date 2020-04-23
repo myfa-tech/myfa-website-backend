538826adf31ff0922f7ba91895bb8871a8d37e6f
Florian Adonis <tanohzana@gmail.com>
2020-04-23 14:55:09 +0000

    fix: add ramadan baskets to basketFactory

--- src/utils/basketFactory.js
+++ src/utils/basketFactory.js
@@ -1,17 +1,19 @@
 
-import baskets from '../assets/baskets';
+import regularBaskets from '../assets/baskets';
 import customBasket from '../assets/customBasket';
+import ramadanBaskets from '../assets/ramadanBaskets';
 
 class BasketFactory {
-  baskets = [...baskets, customBasket];
+  baskets = [...regularBaskets, ...ramadanBaskets, customBasket];
 
   constructor (basketType, userInfo, order, stripeIntentId) {
-    this.structure = baskets.find(b => b.type === basketType);
+    this.structure = this.baskets.find(b => b.type === basketType);
 
     this.basket = {
       name: this.structure.name,
       type: this.structure.type,
       price: this.structure.price,
+      priceCFA: this.structure.priceCFA,
       createdAt: Date.now(),
       recipient: order.recipient,
       status: 'pending',
