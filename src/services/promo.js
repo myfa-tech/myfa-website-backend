f361789e5bd9db77227c555185b8b856fd6f55e9
Florian Adonis <tanohzana@gmail.com>
2020-06-19 18:40:55 +0000

    allow to test promo code

--- src/services/promo.js
+++ src/services/promo.js
@@ -15,4 +15,22 @@ const usePromo = async (code) => {
   }
 };
 
-export { usePromo };
+const testPromoCode = async (req, res, next) => {
+  const promosModel = mongoose.model('promos', PromoSchema);
+  let code = req.query.code;
+
+  try {
+    const promoCode = await promosModel.findOne({ code, used: false });
+
+    if (!!promoCode) {
+      res.status(200).send(true);
+    }
+
+    res.status(400).send(false);
+  } catch(e) {
+    console.log(e);
+    res.status(500).end();
+  }
+};
+
+export { testPromoCode, usePromo };
